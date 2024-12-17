e183434467aef5d2682ea8051eba296c175c4f9e
Mara <Zainullin.marat93@yandex.ru>
2024-12-18 00:42:41 +0300

    111

--- config/settings.py
+++ config/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '111',
         'USER': 'postgres',
         'PASSWORD': '2387',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
