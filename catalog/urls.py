51dd6ca6c93ec6fa333d1135e3744aa015c3484c
Mara <Zainullin.marat93@yandex.ru>
2024-12-20 21:37:11 +0300

    24

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,9 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+from catalog.views import products_list, products_detail
+
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('products_list', products_list, name='products_list'),
+    path('products/<int:pk>/', products_detail, name='products_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from catalog.models import Product
 
 def home(request):
     return render(request, 'catalog/home.html')
@@ -14,3 +15,16 @@ def contacts(request):
 
         return HttpResponse(f'Спасибо, {name}! Сообщение получено')
     return render(request, 'catalog/contacts.html')
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'products_detail.html', context)
