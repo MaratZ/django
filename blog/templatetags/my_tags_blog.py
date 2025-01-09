c938a2e9a697f363a0cfce57583df5d009a96751
Mara <Zainullin.marat93@yandex.ru>
2025-01-09 20:59:54 +0300

    25dz3

--- blog/templatetags/my_tags_blog.py
+++ blog/templatetags/my_tags_blog.py
@@ -2,7 +2,10 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter()
-def media_filter(path):
+def media_filter_blog(path):
     if path:
         return f'/media/{path}'
+    else:
+        return '#'
--- catalog/views.py
+++ catalog/views.py
@@ -19,6 +19,7 @@ class ContactsListView(ListView):
 
 class ProductListView(ListView):
     model = Product
+    template_name = 'catalog/product_list.html'
     context_object_name = 'catalog/product_list/html'
 
 
